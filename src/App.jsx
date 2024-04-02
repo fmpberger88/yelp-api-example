931b80c2fcfdf36fabbf5026bc100774c542337b
fmpberger88 <fabian.berger@protonmail.ch>
2024-04-02 21:21:03 +0200

    initialize project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import Pages from "./views/Page.jsx";
+
+function App() {
+  return (
+      <Pages />
+  )
+}
+
+export default App
---
+++ src/views/Page.jsx
@@ -0,0 +1,8 @@
+
+export default function Pages() {
+    return (
+        <main>
+            <h1>Gleipnir</h1>
+        </main>
+    )
+}
